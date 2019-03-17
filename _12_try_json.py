dedf96c408ef1970004b9a386f0622a77aae265a
“wzzst310” <wzzst310@163.com>
2019-03-18 01:38:48 +0800

    init

--- _12_try_json.py
+++ _12_try_json.py
@@ -21,7 +21,7 @@ ret1 = json.loads(html_str)
 # print(type(ret1))
 # json.dumps能够把python类型转化为json字符串
 with open("douban.json", "w", encoding="utf-8") as f:
-    f.write(json.dumps(ret1, ensure_ascii=False, indent=4)) 
+    f.write(json.dumps(ret1, ensure_ascii=False, indent=4))
     # f.write(str(ret1))
 
 with open("douban.json", "r", encoding="utf-8") as f:
@@ -29,3 +29,13 @@ with open("douban.json", "r", encoding="utf-8") as f:
     ret3 = json.loads(ret2)
     print(ret3)
     print(type(ret3))
+
+# 具有read()或者write()方法的对象就是类文件对象通常叫做fp
+# 使用json.load提取类文件对象中的数据
+with open("douban.json", "r", encoding="utf-8") as f:
+    ret4 = json.load(f)
+    print('ret4 %s' % ret4)
+    print('ret4 type %s' % type(ret4))
+
+with open("douban_dump.json", "w", encoding="utf-8") as f:
+    json.dump(ret4, f, ensure_ascii=False, indent=4)
