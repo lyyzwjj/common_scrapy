890a02fde47d3bf42d7651d403ebb8ab6c35efe4
“wzzst310” <wzzst310@163.com>
2019-04-02 21:49:44 +0800

    init

--- _15_re.py
+++ _15_re.py
@@ -18,3 +18,17 @@ r = re.findall("a[bcd]+e", "abcdeacccddceadeabe")  # 只能取方括号中间的
 print(r)  # ['abcde', 'ace', 'ade', 'abe']
 r = re.findall("abe|ace|ade", "abcdeacccddceadeabe")  # 或者
 print(r)  # ['ade', 'abe']
+r = re.findall("\d", "abcdeacccddceadeabe1234")  # 数字
+print(r)  # ['1', '2', '3', '4']
+r = re.findall("[0-9]", "abcdeacccddceadeabe1234")  # 数字
+print(r)  # ['1', '2', '3', '4']
+r = re.findall("\d+", "abcdeacccddceadeabe1234")  # 数字
+print(r)  # ['1234']
+r = re.findall("\D", "abcdeacccddceadeabe1234")  # 非数字
+print(r)  # ['a', 'b', 'c', 'd', 'e', 'a', 'c', 'c', 'c', 'd', 'd', 'c', 'e', 'a', 'd', 'e', 'a', 'b', 'e']
+r = re.findall("[^\d]", "abcdeacccddceadeabe1234")  # 非数字
+print(r)  # ['a', 'b', 'c', 'd', 'e', 'a', 'c', 'c', 'c', 'd', 'd', 'c', 'e', 'a', 'd', 'e', 'a', 'b', 'e']
+r = re.findall("[\s]", "abcde   acccddcea   deabe1234")  # 非空白字符
+print(r)  # [' ', ' ', ' ', ' ', ' ', ' ']
+r = re.findall("[< >\t\r\n\f\v]", "abcde   acccddcea   deabe1234")  # 非空白字符
+print(r)  # [' ', ' ', ' ', ' ', ' ', ' ']
