a4405689c5a0d8f4cbf6f36ee2a71c73ce92724e
wjj <www.wzzst310@163.com>
2019-04-10 08:05:27 +0800

    百度地图爬虫

--- _21_map_baidu.py
+++ _21_map_baidu.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import demjson
+from openpyxl import Workbook
 
 
 class BaiduMapSpider:
@@ -17,8 +18,26 @@ class BaiduMapSpider:
         response = requests.get(url, headers=self.headers)
         return response.content.decode()
 
-    def save_content_list(self, obj):  # 保存数据
-        print(obj)
+    def save_content_list(self, content_list):  # 保存数据
+        if len(content_list) != 0:
+            wb = Workbook()  # 创建文件对象
+            # grab the active worksheet
+            ws = wb.active  # 获取第一个sheet
+            ws.append(list(content_list[0].keys()))
+            for content in content_list:
+                ws.append(list(content.values()))
+            wb.save(self.city_name + self.kw + ".xlsx")
+        else:
+            print("暂未查到数据")
+        print(content_list)
+
+    def save_addr(self, item, content, name, content_name, *args):
+        if content.get(content_name) is None:
+            item[name] = []
+            for arg in args:
+                item[name].append(content[arg])
+        else:
+            item[name] = content[content_name]
 
     def run(self):
         pn = 0
@@ -29,22 +48,25 @@ class BaiduMapSpider:
             json_str = self.parse_url(self.url_temp.format(pn))
             # 3.提取数据,提取下一页的url地址
             # obj = json.loads(json_str)
-            print(json_str)
             obj = demjson.decode(json_str, encoding="utf-8")
-            if (obj.get("content") is None):
+            if obj.get("content") is not None:
+                for content in obj["content"]:
+                    item = {}
+                    item["name"] = content["name"]
+                    # item["addr"] = content["address_norm"]
+                    self.save_addr(item, content, "addr", "address_norm", "area_name", "addr")
+                    item["navi_x"] = content["navi_x"]
+                    item["navi_y"] = content["navi_y"]
+                    try:
+                        item["phone"] = content["ext"]["detail_info"]["phone"]
+                    except KeyError:
+                        item["phone"] = None
+                    content_list.append(item)
+                pn += 1
+            else:
+                # 4.保存数据
+                self.save_content_list(content_list)
                 pn = -1
-                return
-            for content in obj["content"]:
-                item = {}
-                item["name"] = content["name"]
-                item["addr"] = content["address_norm"]
-                item["navi_x"] = content["navi_x"]
-                item["navi_y"] = content["navi_y"]
-                item["phone"] = content["ext"]["detail_info"]["phone"]
-                content_list.append(item)
-            # 4.保存数据
-            self.save_content_list(content_list)
-            pn += 1
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-dict = {"a": 1}
+dict = {"a": 1, "b": "2"}
+print(list(dict.keys()))
+print(list(dict.values()))
+for key in dict.keys():
+    print(key)
+    print(dict[key])
 print(dict.get("b"))
